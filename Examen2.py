57c94d197660e5b569655302db261c793c272487
Zacomit <80433195+Zacomit@users.noreply.github.com>
2023-11-12 22:07:29 -0600

    Añade 4 pruebas unitarias a Examen2.py

--- Examen2.py
+++ Examen2.py
@@ -37,30 +37,38 @@ print(objeto.DivisibleTempo(10))  # Debería imprimir [1, 2, 5, 10]
 print(objeto.ObtieneMasBailable([0.8, 0.9, 0.7]))  # Debería imprimir 0.9
 print(objeto.VerificaListaCanciones(["Canción 1", "Canción 2", "Canción 3"]))  # Debería imprimir True
 
+################################################################################################
+# Pruebas
+
 class TestMiClase(unittest.TestCase):
 
     def test_obtiene_valencia(self):
         mi_instancia = MiClase(Valencia=1, Tempo=120, Tonos="C", listaCanciones=[...], listaBailabilidad=[...])
         self.assertEqual(mi_instancia.ObtieneValencia(13579), 5)
 
-
+        mi_instancia_otra_prueba = MiClase(Valencia=2, Tempo=150, Tonos="D", listaCanciones=[...], listaBailabilidad=[...])
+        self.assertEqual(mi_instancia_otra_prueba.ObtieneValencia(24680), 0)
 
     def test_divisible_tempo(self):
         mi_instancia = MiClase(Valencia=1, Tempo=120, Tonos="C", listaCanciones=[...], listaBailabilidad=[...])
         self.assertEqual(mi_instancia.DivisibleTempo(7), [1, 7])
 
-
+        mi_instancia_otra_prueba = MiClase(Valencia=2, Tempo=150, Tonos="D", listaCanciones=[...], listaBailabilidad=[...])
+        self.assertEqual(mi_instancia_otra_prueba.DivisibleTempo(12), [1, 2, 3, 4, 6, 12])
 
     def test_obtiene_mas_bailable(self):
         mi_instancia = MiClase(Valencia=1, Tempo=120, Tonos="C", listaCanciones=[...], listaBailabilidad=[...])
         self.assertEqual(mi_instancia.ObtieneMasBailable([0.2, 0.5, 0.8]), 0.8)
 
-
+        mi_instancia_otra_prueba = MiClase(Valencia=2, Tempo=150, Tonos="D", listaCanciones=[...], listaBailabilidad=[...])
+        self.assertIsNone(mi_instancia_otra_prueba.ObtieneMasBailable([]))
 
     def test_verifica_lista_canciones(self):
         mi_instancia = MiClase(Valencia=1, Tempo=120, Tonos="C", listaCanciones=[...], listaBailabilidad=[...])
         self.assertTrue(mi_instancia.VerificaListaCanciones([1, 2, 3]))
 
+        mi_instancia_otra_prueba = MiClase(Valencia=2, Tempo=150, Tonos="D", listaCanciones=[...], listaBailabilidad=[...])
+        self.assertFalse(mi_instancia_otra_prueba.VerificaListaCanciones([1, None, 3]))
 
 
 if __name__ == '__main__':
