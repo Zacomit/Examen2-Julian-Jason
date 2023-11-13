de8c3c0ebda6274ca91d8af3f0998e3b7e9449f3
Jason <jason0101@estudiantec.cr>
2023-11-12 22:00:09 -0600

    Se crean mis pruebas unitarias del archivo Examen2.py

--- Examen2.py
+++ Examen2.py
@@ -1,3 +1,5 @@
+import unittest
+
 class MiClase:
     def __init__(self, Valencia, Tempo, Tonos, listaCanciones, listaBailabilidad):
         self.Valencia = Valencia
@@ -34,3 +36,32 @@ print(objeto.ObtieneValencia(1234567))  # Debería imprimir 4
 print(objeto.DivisibleTempo(10))  # Debería imprimir [1, 2, 5, 10]
 print(objeto.ObtieneMasBailable([0.8, 0.9, 0.7]))  # Debería imprimir 0.9
 print(objeto.VerificaListaCanciones(["Canción 1", "Canción 2", "Canción 3"]))  # Debería imprimir True
+
+class TestMiClase(unittest.TestCase):
+
+    def test_obtiene_valencia(self):
+        mi_instancia = MiClase(Valencia=1, Tempo=120, Tonos="C", listaCanciones=[...], listaBailabilidad=[...])
+        self.assertEqual(mi_instancia.ObtieneValencia(13579), 5)
+
+
+
+    def test_divisible_tempo(self):
+        mi_instancia = MiClase(Valencia=1, Tempo=120, Tonos="C", listaCanciones=[...], listaBailabilidad=[...])
+        self.assertEqual(mi_instancia.DivisibleTempo(7), [1, 7])
+
+
+
+    def test_obtiene_mas_bailable(self):
+        mi_instancia = MiClase(Valencia=1, Tempo=120, Tonos="C", listaCanciones=[...], listaBailabilidad=[...])
+        self.assertEqual(mi_instancia.ObtieneMasBailable([0.2, 0.5, 0.8]), 0.8)
+
+
+
+    def test_verifica_lista_canciones(self):
+        mi_instancia = MiClase(Valencia=1, Tempo=120, Tonos="C", listaCanciones=[...], listaBailabilidad=[...])
+        self.assertTrue(mi_instancia.VerificaListaCanciones([1, 2, 3]))
+
+
+
+if __name__ == '__main__':
+    unittest.main()
